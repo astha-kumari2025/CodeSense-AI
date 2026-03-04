3c955e505fea73b0b15d68070031deb0ce607275
astha-kumari2025 <asthakumari6569@gmail.com>
2026-03-04 21:16:52 +0530

    Add AI code explanation function using OpenAI API

---
+++ utils/ai_explainer.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def explain_code(code):
+
+    prompt = f"""
+    Explain the following code in very simple language for beginners.
+    Also mention what the code is doing step by step.
+
+    Code:
+    {code}
+    """
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return response.choices[0].message.content
